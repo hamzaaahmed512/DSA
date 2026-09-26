0c5154873fd5d451604fb515fba1e14a00275691
Hamza Ahmed <hamzaaahmed512@gmail.com>
2026-09-26 13:12:10 +0500

    Time: 0 ms (100%), Space: 42.1 MB (12.67%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int frequency = 0;
+        int ans = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (frequency == 0) {
+                ans = nums[i];
+                frequency = 1;
+            } else if (ans == nums[i]) {
+                frequency++;
+            } else {
+                frequency--;
+            }
+        }
+
+        return ans;
+    }
+};
